cff9ac272c9a3c220343601924e52a4b67488852
joker69216 <hamaraad883@gmail.com>
2022-12-03 22:28:24 +0300

    add styles to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "./App.css";
+
+const Component = ({ value, language }) => {
+  return (
+    <SyntaxHighlighter language={language ?? "javascript"} style={dark}>
+      {value ?? "nothing"}
+    </SyntaxHighlighter>
+  );
+};
 
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
-    <div className="App" />
-
+    <div className="App">
+      <div className="left">
+        <div className="appBar"></div>
+        <textarea
+          type="text"
+          value={input}
+          onChange={handleInput}
+          id="editor"
+          autoFocus
+        />
+      </div>
+      <div className="right">
+        <div className="appBar"></div>
+        <div id="preview">
+          <ReactMarkdown
+            children={input}
+            renderers={{
+              code: Component,
+            }}
+          ></ReactMarkdown>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default App;
+
+
