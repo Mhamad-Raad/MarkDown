7c0180803074280a0e90a16a932eacb87dc2e060
joker69216 <hamaraad883@gmail.com>
2022-12-03 23:00:07 +0300

    adjust the design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "./App.css";
 
@@ -15,6 +14,48 @@ const Component = ({ value, language }) => {
 
 function App() {
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const a = {
+
+    }
+    setInput(`# Welcome 'React Markdown Previewer!'
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+\`\`\` 
+// this is multi-line code:
+\`\`\`
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy.
+
+And here is a horizzantal line !!!
+
+
+There's also [links](https://www.linkedin.com/in/mhamad-raad), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+- And of course there are lists.
+- Some are bulleted.
+- With different indentation levels.
+- That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. you can Get Horizanl Lines:
+---
+1. And last but not least, let's not forget embedded images:
+
+![Image](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHpTun6ZJzXtt0rIiRtvI5Hdt0JwtGFxxmcA&usqp=CAU)
+`);
+  }, []);
 
   const handleInput = (e) => {
     setInput(e.target.value);
@@ -52,6 +93,8 @@ function App() {
 }
 
 export default App;
+
+
 
 
 
